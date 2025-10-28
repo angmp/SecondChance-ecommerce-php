eb027edc70f89dfd705c5ca153793cf5672cac5e
Anggi Magdalena <104726534+angmp@users.noreply.github.com>
2025-10-28 15:04:00 +0700

    Refactor db_connect.php to use environment variables
    Updated database connection to use environment variables for configuration.

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,20 @@
 <?php
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "sc_db"; 
+// Ambil data koneksi dari environment Railway
+$servername = getenv("MYSQLHOST");
+$username   = getenv("MYSQLUSER");
+$password   = getenv("MYSQLPASSWORD");
+$dbname     = getenv("MYSQLDATABASE");
+$port       = getenv("MYSQLPORT"); // optional, tapi disarankan
 
 // Membuat koneksi
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Mengecek koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+
 
 
